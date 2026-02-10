bc302b26f219408961b2f9fbd41112ce17929d86
ItxUmeer <umerchattha.dxb@icloud.com>
2026-02-11 01:12:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
- /images
-      dubai-skyline.jpg
-      dubai-skyline-night.jpg
-      smart-dubai.svg
-      mohre.svg
-      icp.svg
+function toggleMenu() {
+  document.getElementById("navMenu").classList.toggle("active");
+}
+
+/* COUNTER ANIMATION */
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter => {
+  let target = +counter.dataset.target;
+  let count = 0;
+  let inc = target / 100;
+
+  function update() {
+    count += inc;
+    if (count < target) {
+      counter.innerText = Math.ceil(count);
+      setTimeout(update, 20);
+    } else {
+      counter.innerText = target;
+    }
+  }
+  update();
+});
