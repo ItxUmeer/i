e86b8bfaa9c4dd9cdc6e6e2a04c7d49223b250bc
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 13:46:18 +0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,39 +1,17 @@
-import { db, auth } from "./firebase.js";
-import { signInWithEmailAndPassword } 
-from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import { auth, db } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 
-import { doc, getDoc } 
-from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+export async function login(email, password) {
 
-signInWithEmailAndPassword(auth, email, password)
-.then(async (userCred) => {
-
+  const userCred = await signInWithEmailAndPassword(auth, email, password);
   const uid = userCred.user.uid;
 
-  const userSnap = await getDoc(doc(db, "users", uid));
+  const snap = await getDoc(doc(db, "users", uid));
 
-  if (!userSnap.exists()) {
-    alert("User role not found");
-    return;
+  if (!snap.exists()) {
+    throw new Error("User profile not found");
   }
 
-  const role = userSnap.data().role;
-
-  // ROLE ROUTING
-  if (role === "admin") {
-    window.location.href = "dashboard.html";
-  }
-
-  else if (role === "assets_officer") {
-    window.location.href = "assets.html";
-  }
-
-  else if (role === "facilities_supervisor") {
-    window.location.href = "supervisor.html";
-  }
-
-  else {
-    alert("Access denied: Invalid role");
-  }
-
-});
+  return snap.data().role;
+}
