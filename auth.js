d632c246903a68ab711d4e87c59ce2581f6f0f43
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 11:04:21 +0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,20 +5,21 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
+// LOGIN
 window.login = function () {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
 
   signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
     .catch(e => alert(e.message));
 };
 
+// LOGOUT (GLOBAL FIX)
 window.logout = function () {
-  signOut(auth);
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
+  });
 };
-
-onAuthStateChanged(auth, user => {
-  if (user) {
-    window.location.href = "dashboard.html";
-  }
-});
