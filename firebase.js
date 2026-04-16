ec0d7c94851aaa684011440e03abef8dea41b29d
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 09:10:46 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyASPK51x0QaqT_DJHasx0bn3Cww2Q5j0_A",
+  authDomain: "mep-maintenance-system.firebaseapp.com",
+  projectId: "mep-maintenance-system",
+  storageBucket: "mep-maintenance-system.firebasestorage.app",
+  messagingSenderId: "449309293495",
+  appId: "1:449309293495:web:15c7cfcf0293308b388632"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
