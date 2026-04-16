aa0ad8a7bcc1875cb78109d3d814892762aab8cc
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 11:58:15 +0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,25 +1,39 @@
-import { auth } from "./firebase.js";
-import {
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import { db, auth } from "./firebase.js";
+import { signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
-// LOGIN
-window.login = function () {
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+import { doc, getDoc } 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "dashboard.html";
-    })
-    .catch(e => alert(e.message));
-};
+signInWithEmailAndPassword(auth, email, password)
+.then(async (userCred) => {
 
-// LOGOUT (GLOBAL FIX)
-window.logout = function () {
-  signOut(auth).then(() => {
-    window.location.href = "index.html";
-  });
-};
+  const uid = userCred.user.uid;
+
+  const userSnap = await getDoc(doc(db, "users", uid));
+
+  if (!userSnap.exists()) {
+    alert("User role not found");
+    return;
+  }
+
+  const role = userSnap.data().role;
+
+  // ROLE ROUTING
+  if (role === "admin") {
+    window.location.href = "dashboard.html";
+  }
+
+  else if (role === "assets_officer") {
+    window.location.href = "assets.html";
+  }
+
+  else if (role === "facilities_supervisor") {
+    window.location.href = "supervisor.html";
+  }
+
+  else {
+    alert("Access denied: Invalid role");
+  }
+
+});
