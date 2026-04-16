0253aeca6339d6e1bc4db536dd6261a08947dc60
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 10:10:50 +0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA3PK51x0QAqT_DJHasx0bn3Cww2Q5j0_A",
+    authDomain: "mep-maintenance-system.firebaseapp.com",
+    projectId: "mep-maintenance-system",
+    storageBucket: "mep-maintenance-system.firebasestorage.app",
+    messagingSenderId: "449309293495",
+    appId: "1:449309293495:web:061124ae9f1c990e388632"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
