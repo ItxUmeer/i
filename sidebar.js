90d46a714d3e9d1b88885e151303829af9a54b9f
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 13:46:53 +0400

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,58 +1,27 @@
-import { db, auth } from "./firebase.js";
-import { doc, getDoc } 
-from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+export function renderSidebar(role) {
 
-import { signOut } 
-from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+  const menu = document.getElementById("menu");
 
-async function loadSidebar() {
-
-  const user = auth.currentUser;
-  if (!user) return;
-
-  const snap = await getDoc(doc(db, "users", user.uid));
-
-  if (!snap.exists()) return;
-
-  const role = snap.data().role;
-
-  let menu = "";
+  let html = `<a href="dashboard.html">Dashboard</a>`;
 
   if (role === "admin") {
-    menu = `
-      <button onclick="location.href='dashboard.html'">Dashboard</button>
-      <button onclick="location.href='assets.html'">Assets</button>
-      <button onclick="location.href='workorders.html'">Work Orders</button>
-      <button onclick="location.href='ppm.html'">PPM</button>
-      <button onclick="location.href='supervisor.html'">Facilities Supervisor</button>
-    `;
+    html += `<a href="workorders.html">Work Orders</a>`;
+    html += `<a href="assets.html">Assets</a>`;
+    html += `<a href="ppm.html">PPM</a>`;
   }
 
-  else if (role === "assets_officer") {
-    menu = `
-      <button onclick="location.href='assets.html'">Assets</button>
-      <button onclick="location.href='ppm.html'">PPM</button>
-    `;
+  if (role === "facilities_supervisor") {
+    html += `<a href="workorders.html">Approve Work Orders</a>`;
+    html += `<a href="ppm.html">PPM</a>`;
   }
 
-  else if (role === "facilities_supervisor") {
-    menu = `
-      <button onclick="location.href='supervisor.html'">My Work Orders</button>
-      <button onclick="location.href='ppm.html'">PPM Schedule</button>
-    `;
+  if (role === "assets_officer") {
+    html += `<a href="assets.html">Assets</a>`;
   }
 
-  document.querySelector(".sidebar").innerHTML = `
-    <h2>CMMS Dubai</h2>
-    ${menu}
-    <hr style="border:1px solid #1f2937">
-    <button id="logoutBtn">Logout</button>
-  `;
+  if (role === "maintenance") {
+    html += `<a href="workorders.html">My Work Orders</a>`;
+  }
 
-  document.getElementById("logoutBtn").onclick = () => {
-    signOut(auth);
-    window.location.href = "index.html";
-  };
+  menu.innerHTML = html;
 }
-
-loadSidebar();
