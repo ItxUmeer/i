d0a18d3b7a50f48fa4c8ec94ed66af42260ee36c
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 09:30:39 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
-// Import Firebase SDKs (CDN version)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyASPK51x0QaqT_DJHasx0bn3Cww2Q5j0_A",
   authDomain: "mep-maintenance-system.firebaseapp.com",
@@ -12,8 +10,5 @@ const firebaseConfig = {
   appId: "1:449309293495:web:15c7cfcf0293308b388632"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export services
 export const db = getFirestore(app);
