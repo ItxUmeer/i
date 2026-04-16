2d8ab02d1645356e24b558af1c858c03a9d2a87e
ItxUmeer <umerchattha.dxb@icloud.com>
2026-04-16 11:58:42 +0400

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,18 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const sidebarHTML = `
-    <div class="sidebar">
-      <h2>Dubai CMMS</h2>
+import { db, auth } from "./firebase.js";
+import { doc, getDoc } 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 
+import { signOut } 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+
+async function loadSidebar() {
+
+  const user = auth.currentUser;
+  if (!user) return;
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+
+  if (!snap.exists()) return;
+
+  const role = snap.data().role;
+
+  let menu = "";
+
+  if (role === "admin") {
+    menu = `
       <button onclick="location.href='dashboard.html'">Dashboard</button>
       <button onclick="location.href='assets.html'">Assets</button>
       <button onclick="location.href='workorders.html'">Work Orders</button>
       <button onclick="location.href='ppm.html'">PPM</button>
+      <button onclick="location.href='supervisor.html'">Facilities Supervisor</button>
+    `;
+  }
 
-      <hr style="border:1px solid #1f2937">
+  else if (role === "assets_officer") {
+    menu = `
+      <button onclick="location.href='assets.html'">Assets</button>
+      <button onclick="location.href='ppm.html'">PPM</button>
+    `;
+  }
 
-      <button onclick="logout()">Logout</button>
-    </div>
+  else if (role === "facilities_supervisor") {
+    menu = `
+      <button onclick="location.href='supervisor.html'">My Work Orders</button>
+      <button onclick="location.href='ppm.html'">PPM Schedule</button>
+    `;
+  }
+
+  document.querySelector(".sidebar").innerHTML = `
+    <h2>CMMS Dubai</h2>
+    ${menu}
+    <hr style="border:1px solid #1f2937">
+    <button id="logoutBtn">Logout</button>
   `;
 
-  document.getElementById("sidebar").innerHTML = sidebarHTML;
-});
+  document.getElementById("logoutBtn").onclick = () => {
+    signOut(auth);
+    window.location.href = "index.html";
+  };
+}
+
+loadSidebar();
