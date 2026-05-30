7c1dd3e0af4725b92aefd704b5afb7bfc8ebd4b6
ItxUmeer <umerchattha.dxb@icloud.com>
2026-05-30 19:55:50 +0400

    Update roleguard.js

--- roleguard.js
+++ roleguard.js
@@ -1,17 +1,22 @@
 import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
-export function checkAccess() {
+export function checkAccess(callback){
 
-    const role =
-        localStorage.getItem("userRole");
+    onAuthStateChanged(auth,(user)=>{
 
-    if (!auth.currentUser) {
+        if(!user){
 
-        window.location.href =
-            "login.html";
+            window.location.href="login.html";
+            return;
 
-        return;
-    }
+        }
 
-    return role;
+        const role =
+            localStorage.getItem("userRole");
+
+        callback(role);
+
+    });
+
 }
